7e56f278f2cb3c2c8497c9617101f74853805e14
huynhben <benhuynh46@gmail.com>
2024-08-01 14:30:01 -0400

    added text to center of site

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
 import { Box } from "@mui/material";
 
-export default function Home() {
-  return <Box
-  width="100vw"
-  
-  ></Box>
+const item = 
+[
+  'tomato',
+  'bannana',
+  'carrot',
+  'mango',
+  'pineapple'
 
+]
+
+export default function Home() {
+  return (
+    <Box
+    width="100vw"
+    height="100vh"
+    alignItems={"center"}
+    justifyContent={"center"}
+    display={"flex"}
+  
+    >
+      Hi
+    </Box>
+  )
 }
